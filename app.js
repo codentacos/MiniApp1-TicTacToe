65d6c0537ac26854b2b76d6bc7c855fbd7c2a4e5
Zachary Nelson <nelsonz2013@hotmail.com>
2020-07-27 12:46:28 -0500

    Handles player turns correctly, no longer allows moves in spots that have been played already

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-// first move is always X
-// detect win or tie and display message
 // must have reset button
 
+//----------------------------------------------//
+// MODEL - HANDLES DATA AND STATE OF GAME
+//----------------------------------------------//
+let currentMove = 'x-class';
+// first move is always X
+// alternate player icon each turn
+// detect win or tie and display message
 
-let td = document.getElementsByTagName('td');
+let handleTurn = () => {
+  if (currentMove === 'x-class') {
+    currentMove = 'o-class';
+  } else {
+    currentMove = 'x-class';
+  }
+  
+}
+
+//----------------------------------------------//
+// CONTROLLER - HANDLES USER INTERACTIONS
+//----------------------------------------------//
+
+const td = document.getElementsByTagName('td');
 
 // add click events to each square
 for (let i = 0; i < td.length; i++) {
   td[i].addEventListener('click', (event) => {
-    console.log(event.currentTarget.id)
+    // adds the current players move to the board
+    if (event.currentTarget.classList.value === '') {
+      event.currentTarget.classList.add(currentMove);
+      handleTurn();
+    }
+    
   });
 }
+
